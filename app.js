19141472ef2fbecd65ca9e1f432c404b5cce906f
Ethan Smith <ethan.smith@biomimicry.net>
2015-01-27 11:40:06 -0800

    Change to hello scott

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello jenkins');
+  res.send('hello scott');
 });
 
 app.listen(process.env.PORT || 5000);
