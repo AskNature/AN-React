5539c701a1311a393859aa452713783d6a675c93
Scott Halstvedt <shalstvedt@gmail.com>
2015-02-13 05:42:58 -0500

    provide proper status codes on local auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,7 +11,7 @@ var passport = require('passport');
 var routes = function (app) {
   app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'] }));
   app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login', successRedirect: '/' }));
-  app.post('/auth/db', passport.authenticate('local', { failureRedirect: '/login', successRedirect: '/'}));
+  app.post('/auth/db', passport.authenticate('local'), function(req, res) { res.send(); });
 };
 
 module.exports = routes;
