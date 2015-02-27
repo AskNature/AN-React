dea528b1a1aff0b3721d842a10de8b9458d2d03f
Scott Halstvedt <shalstvedt@gmail.com>
2015-02-27 00:15:34 -0500

    updated conditions console to use griddle

--- client/scripts/components/admin/conditions.jsx
+++ client/scripts/components/admin/conditions.jsx
@@ -1,87 +1,24 @@
 /**
-* PhenomenaConsole Component
+* Conditions Console Component
 */
 'use strict';
 
 var React = require('react');
 var DefaultLayout = require('../layouts/default.jsx');
-var focusStore = require('../../stores/admin/conditions');
-var focusActions = require('../../actions/conditions');
+var conditionStore = require('../../stores/admin/conditions');
+var conditionActions = require('../../actions/conditions');
+var GriddleComponent = require('./griddle_component.jsx');
 
-var getState = function() {
-  return {
-    items: focusStore.get()
-  };
-};
-
-var initializeTable = function(state) {
-  var listitems = state.items.results;
-  $('#list').dataTable({
-   'destroy': true,
-   'data': listitems,
-   stateSave: true,
-   'language': {
-      search: '',
-      searchPlaceholder: 'Filter',
-      'lengthMenu': '_MENU_'
-    },
-    'columns' : [
-      {'data':'name','title': 'Name', 'render': function(data,type,row) {
-          var url = '../condition/'+row.masterid;
-          return '<a href="'+url+'"><strong>'+data+'</strong></a>';
-        }
-      }
-    ]
- });
- $('.dataTables_filter input[type="search"], .dataTables_length select').addClass('form-control input-lg');
-};
-
-/** ItemsFilter class contains a search field that filters items in
-* an unordered list in real time.
-*/
-
-var FocusTable = React.createClass({
-
-  mixins: [focusStore.mixin],
-
-  getInitialState: function() {
-    return getState();
-  },
-
-  componentDidMount: function(){
-    focusActions.getList();
-    initializeTable(this.state);
-  },
-
-  render: function() {
-    return (
-      <div>
-        <table className='table display' id='list'>
-
-        </table>
-        <button className='btn btn-primary' onClick={this.handleClick} label="Reset">Reset</button>
-      </div>
-    );
-  },
-
-  handleClick: function() {
-    focusActions.getList();
-  },
-  // Event handler for 'change' events coming from store mixins.
-  _onChange: function() {
-      this.setState(getState());
-      initializeTable(this.state);
-  }
-});
-
-var FocusConsole = React.createClass({
+var ConditionConsole = React.createClass({
     render: function() {
         return (
             /* jshint ignore:start */
             <DefaultLayout>
                 <div className="main-container">
                         <h1>Challenge Console</h1>
-                        <FocusTable />
+                        <GriddleComponent store={conditionStore} actions={conditionActions}
+                        linkColumnName={"name"}
+                        columns={["name", "masterid"]} />
                 </div>
             </DefaultLayout>
             /* jshint ignore:end */
@@ -89,4 +26,4 @@ var FocusConsole = React.createClass({
     }
 });
 
-module.exports = FocusConsole;
+module.exports = ConditionConsole;
