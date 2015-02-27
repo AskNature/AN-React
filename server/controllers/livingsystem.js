6b7a92eac322d05369f8959246f5e22f9a8bb645
Scott Halstvedt <shalstvedt@gmail.com>
2015-02-26 23:24:33 -0500

    updated living systems api with limit, offset, order, and caching

--- server/controllers/livingsystem.js
+++ server/controllers/livingsystem.js
@@ -3,25 +3,62 @@ var db = require('../config/database').db,
 settings = require('../config/env/default'),
 path = require('path');
 
+var Cached = require('cached');
+
+var livingSystemCache;
+if(process.env.NODE_ENV == 'production') {
+    livingSystemCache = Cached('livingSystem', { backend: {
+        type: 'memcached',
+        hosts: '127.0.01:11211'
+    }});
+} else {
+    livingSystemCache = Cached('livingSystem');
+}
+livingSystemCache.setDefaults({"freshFor": 120});
+
+
 var loadindex = function(req, res, next) {
   // Render index.html to allow application to handle routing
    res.sendFile(path.join(settings.staticAssets, '/index.html'), { root: settings.root });
 };
 
 var returnList = function(req, res) {
-  db
-  .select('name, masterid')
+  var chain = db
+  .select('name, masterid, "living-system" as entityType')
   .from('LivingSystem')
-  .where('in_HasLivingSystem IS NOT NULL')
-  .limit('200')
-  .all()
-  .then(function (results) {
-      res.status(200).json({
-        results: results
-      });
-      console.log('The collection controller has sent ' + results.length + ' records.');
-  })
-  .done();
+  .where('in_HasLivingSystem IS NOT NULL');
+
+  var limit = parseInt(req.query["limit"]);
+  if(limit) {
+      chain.limit(limit);
+  }
+
+  var offset = parseInt(req.query["offset"]);
+  if(offset) {
+      chain.offset(offset);
+  }
+
+  var order = req.query["order"];
+  if(order) {
+      chain.order(order.substring(1) + (order.substring(0,1)=="-" ? "desc" : "asc"));
+  }
+
+  livingSystemCache.getOrElse('count', Cached.deferred(function(done) {
+      db.select('count(*)').from('LivingSystem')
+      .where('in_HasLivingSystem IS NOT NULL')
+      .scalar().then(function(count) {
+          done(null, count); // return Cached.deferred
+      }).done();
+  })).then(function(count) {
+      chain.all().then(function (results) {
+	  res.status(200).json({
+              results: results,
+	      count: count,
+	      maxPages: Math.ceil(count/limit)
+	  });
+	  console.log('The living system API has sent ' + results.length + ' records.');
+      }).done();
+  });
 };
 
 var returnItem = function(req, res, next) {
