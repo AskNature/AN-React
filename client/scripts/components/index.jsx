02ba622d6c87a8658d02d619093361308df1f8d7
Ethan Smith <ethan.smith@biomimicry.net>
2015-02-27 08:58:30 -0800

    Update landing component copy

--- client/scripts/components/index.jsx
+++ client/scripts/components/index.jsx
@@ -15,9 +15,11 @@ var IndexComponent = React.createClass({
                 <div className="main-container">
                   <div className="container">
                     <div className="jumbotron">
-                    <h1>Welcome to the AskNature Incubator!</h1>
-                    <p>Be warned, this is in a very early stage of development. Feel free to poke around, but please understand that everything is still rough and many things won't work.</p>
-                    <p><a className="btn btn-primary btn-lg" href="http://www.asknature.org" role="button">Go to the stable version of AskNature.org</a></p>
+                    <h2>Welcome to the AskNature 2 Incubator!</h2>
+                    <p>Be warned, this site is in a <strong>very early stage of development</strong>. We welcome you to check it out, but be warned that everything is still very rough and many things won't work.</p>
+                    <p>Cheers, The AskNature Team</p>
+                    <p><a href="http://www.biomimicry.org" target="_blank">The Biomimicry Institute</a></p>
+                    <p><a className="btn btn-primary btn-lg" href="http://www.asknature.org" role="button">Go to AskNature.org</a></p>
                   </div>
                 </div>
                 </div>
