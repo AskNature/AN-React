6740120c75e0a07651923a56d4766fd292d3395f
Scott Halstvedt <shalstvedt@gmail.com>
2015-03-04 20:56:30 -0500

    added all product fields

--- server/models/product.js
+++ server/models/product.js
@@ -4,7 +4,7 @@ var Model = require('./model.js');
 
 var entityName = 'InspiredSolutions';
 
-var fields = ['name']
+var fields = ['name', 'headline', 'special_text', 'challenges_solved', 'how_is_it_different', 'biomimicry_story', 'product_type', 'patent_name', 'availability', 'company', 'phase', 'patent_number', 'company_website', 'strategy', 'consumer_products', 'keywords', 'status', 'timestamp'];
 
 var Product = Model(entityName, fields);
 
