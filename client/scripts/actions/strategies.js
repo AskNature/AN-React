fc7bedadd964a3e67ca90ae8cdcc0b5f731e5869
Scott Halstvedt <shalstvedt@gmail.com>
2015-03-09 20:14:31 -0400

    updated strategy actions

--- client/scripts/actions/strategies.js
+++ client/scripts/actions/strategies.js
@@ -8,6 +8,96 @@ var Constants = require('../constants/strategy');
 var Defaults = require('../constants/defaults').strategy;
 var request = require('superagent');
 var assign = require('object-assign');
+var _ = require('lodash');
+
+/**
+* initialize begins the load of a strategy detail instance. It takes
+* seed data and either empties the store if the data is null or
+* fills it with the seed data (from a list or from a dehydrated store).
+*/
+
+var initialize = function(initialData) {
+    Dispatcher.handleViewAction({
+	actionType: Constants.INTIALIZE_STRATEGY,
+	data: assign({}, Defaults, initialData)
+    });
+};
+
+/**
+* fetch begins to fetch additional data for the object from the server. It takes
+* the masterid of the object
+*/
+
+var fetch = function(masterid) {
+    // do the async fetch with masterid
+    var self = this;
+    Dispatcher.handleViewAction({
+	actionType: Constants.FETCH_STRATEGY
+    });
+    request
+    .get('/api/v2/strategies/'+id)
+    .type('json')
+    .end(function(res) {
+      if (res.ok) {
+        if (res) {
+          var itemData = res.body;
+            Dispatcher.handleViewAction({
+		actionType: Constants.FETCH_STRATEGY_SUCCESS,
+		data: data
+	    });
+        }
+      }
+      else {
+	  Dispatcher.handleViewAction({
+	      actionType: Constants.FETCH_STRATEGY_ERROR
+	  });
+      }
+    });
+};
+
+/**
+* update takes new user data from the view and updates the model in the background.
+* It takes a map of the new data, and doesn't update the server until commit is called.
+*/
+
+var update = function(data) {
+    Dispatcher.handleViewAction({
+	actionType: Constants.UPDATE_STRATEGY,
+	data: data
+    });
+};
+
+/**
+* commit submits a set of changes to the server, and marks them as saved in the store
+*/
+
+var commit = function(fields) {
+    Dispatcher.handleViewAction({
+	actionType: Constants.COMMIT_STRATEGY,
+	fields: fields
+    });
+    var self = this;
+    var masterid = store.getId();
+    var changedData = _.pick(store.getUpdatedFields(), fields);
+    request
+    .post('/api/v2/strategies/'+data.masterid)
+    .send(data)
+    .end(function(res) {
+        if(res.ok) {
+	    Dispatcher.handleViewAction({
+		actionType: Constants.COMMIT_STRATEGY_SUCCESS,
+		fields: fields,
+		data: res
+	    });
+        } else {
+	    Dispatcher.handleViewAction({
+		actionType: Constants.COMMIT_STRATEGY_ERROR,
+		fields: fields,
+		error: res
+	    });
+	}
+    });
+};
 
 module.exports = {
 
