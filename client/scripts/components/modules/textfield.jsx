7146e7cc636077ca6393cd579ebb2b7c5c12281b
Scott Halstvedt <shalstvedt@gmail.com>
2015-03-26 20:52:38 -0400

    added Scribe placeholders and fixed Scribe editing when empty

--- client/scripts/components/modules/textfield.jsx
+++ client/scripts/components/modules/textfield.jsx
@@ -6,12 +6,7 @@ TextField = React.createClass({
         return (
             <div style={this.props.editable? {}:{"borderBottom": "1px solid transparent"}}>
                 {this.props.editable ?
-                    <Scribe
-                        initialValue={this.props.initialValue}
-                        store={this.props.store}
-                        actions={this.props.actions}
-                        enableBlockMode={this.props.enableBlockMode}
-                        fieldName={this.props.fieldName} />
+                    <Scribe {...this.props} />
                 : this.props.initialValue}
             </div>
         );
