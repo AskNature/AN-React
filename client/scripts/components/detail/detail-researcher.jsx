e66bc215ef62ea38065c442e9a70a946c99ca5d4
Ethan Smith <ethan.smith@biomimicry.net>
2015-03-26 21:28:06 -0700

    convert remaining detail-xxx components to use topsection component

--- client/scripts/components/detail/detail-researcher.jsx
+++ client/scripts/components/detail/detail-researcher.jsx
@@ -4,14 +4,11 @@ var React = require('react'),
 
 Link = require('../modules/link.jsx'),
 DefaultLayout = require('../layouts/default.jsx'),
-Hero = require('./common/hero.jsx'),
-SubHero = require('./common/subhero.jsx'),
-AdminBar = require('./common/adminbar.jsx'),
-CreatorMast = require('./common/creatormast.jsx'),
+
+TopSection = require('./common/topsection.jsx'),
+
 TextArea = require('./common/textarea.jsx'),
 DataTable = require('./common/datatable.jsx'),
-ImageList = require('./common/imagelist.jsx'),
-ButtonList = require('./common/edgelists.jsx'),
 Gallery = require('./common/gallery.jsx'),
 RelationshipList = require('./common/relationshiplist.jsx'),
 
@@ -20,6 +17,8 @@ PanelGroup = require('react-bootstrap/PanelGroup'),
 Col = require('react-bootstrap/Col'),
 Row = require('react-bootstrap/Row'),
 Grid = require('react-bootstrap/Grid');
+
+var Select = require('react-select');
 
 var Template = React.createClass({
 
@@ -30,33 +29,15 @@ var Template = React.createClass({
     return (
       /* jshint ignore:start */
       <div>
-        <AdminBar
-          masterid={this.props.masterid}
+        <TopSection
+          {...this.props}
           routename={routeNameSingle}
-          pluralroute={this.props.type}
-          entityname={entityName} />
-        <CreatorMast
-          img="https://lh5.googleusercontent.com/-rybUadmgv5g/AAAAAAAAAAI/AAAAAAAAABA/LDHYA7EFTuI/s120-c/photo.jpg"
-          entityname={entityName} />
-        <Hero
-          editable={this.props.editable}
-          store={this.props.store}
-          actions={this.props.actions}
-          media={data.media}
-          primarytitle={data.name}
-          secondarytitle={data.institution}
+          entityName={entityName}
+          primarytitle={this.props.data.name}
+          secondarytitle={this.props.data.institution}
           secondarylink=''
-          masterid={this.props.masterid} />
-        <SubHero
-          description={data.summary}
-          descriptionlink={''}
-          editable={this.props.editable}
-          store={this.props.store}
-          actions={this.props.actions}
-          editBegin={this.props.editBegin}
-          editFinish={this.props.editFinish}
-          editCancel={this.props.editCancel}
-          onDelete={this.props.onDelete}/>
+          description={this.props.data.summary}
+          />
 
           {this.props.userrole == 'admin' || 'editor' ? (
             <PanelGroup defaultActiveKey='0' accordion>
