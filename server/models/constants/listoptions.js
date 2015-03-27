349b60cf0f1942718052557db9a43c66cef5c703
Ethan Smith <ethan.smith@biomimicry.net>
2015-03-26 22:35:48 -0700

    change order of contentstatus constants list

--- server/models/constants/listoptions.js
+++ server/models/constants/listoptions.js
@@ -2,13 +2,13 @@
 
 var ContentStatus = [
     {label: 'Published', masterid:'published'},
-    {label: 'Duplicate', masterid:'duplicate'},
-    {label: 'Flagged for Review', masterid:'flagged-for-review'},
-    {label: 'Spam', masterid:'spam'},
-    {label: 'Under Review', masterid:'under-review'},
-    {label: 'Archived', masterid:'archived'},
     {label: 'Draft', masterid:'draft'},
     {label: 'Ready for Review', masterid:'ready-for-review'},
+    {label: 'Flagged for Review', masterid:'flagged-for-review'},
+    {label: 'Under Review', masterid:'under-review'},
+    {label: 'Archived', masterid:'archived'},
+    {label: 'Spam', masterid:'spam'},
+    {label: 'Duplicate', masterid:'duplicate'},
     {label: 'Incomplete Entry', masterid:'incomplete-entry'}
 ];
 
