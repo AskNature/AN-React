95788138630d92166ce6901e23d9ca1b50043ea7
Ethan Smith <ethan.smith@biomimicry.net>
2015-04-08 14:38:25 -0700

    add researcher to autocomplete

--- server/routes/autocomplete.js
+++ server/routes/autocomplete.js
@@ -10,10 +10,14 @@ var StrategyController = require('../controllers/bstrategy');
 
 var DStrategy = require('../models/dstrategy.js');
 var BStrategy = require('../models/bstrategy.js');
+
 var DSystem = require('../models/dsystem.js');
 var BSystem = require('../models/bsystem.js');
+
 var FM = require('../models/fm.js');
 var Context = require('../models/context.js');
+
+var Researcher = require('../models/researcher.js');
 
 var returnAutocomplete = function(model) {
     return function(req, res, next) {
@@ -27,10 +31,14 @@ var routes = function (app) {
   // API v2
   app.get('/api/v2/autocomplete/d.strategy', returnAutocomplete(DStrategy));
   app.get('/api/v2/autocomplete/b.strategy', returnAutocomplete(BStrategy));
+
   app.get('/api/v2/autocomplete/d.system', returnAutocomplete(DSystem));
   app.get('/api/v2/autocomplete/b.system', returnAutocomplete(BSystem));
+
   app.get('/api/v2/autocomplete/fm', returnAutocomplete(FM));
   app.get('/api/v2/autocomplete/context', returnAutocomplete(Context));
+
+  app.get('/api/v2/autocomplete/researcher', returnAutocomplete(Researcher));
 
 };
 
