38f5993b74ae676e2fca68505edd716980e7b589
Scott Halstvedt <shalstvedt@gmail.com>
2015-04-15 18:24:23 -0400

    instrumentation

--- server/routes/test.js
+++ server/routes/test.js
@@ -9,6 +9,7 @@ var Controller = require('../controllers/test');
 var routes = function (app) {
   app.get('/test', Controller.testController);
   app.get('/test/:query', Controller.testControllerQuery);
+  app.get('/testuser/:id', Controller.testUser);
 };
 
 module.exports = routes;
