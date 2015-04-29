44355c454b444aadf0035e91799068c020eaaaca
Scott Halstvedt <shalstvedt@gmail.com>
2015-04-28 20:00:02 -0400

    removed debugging S3 calls from server config

--- server/config/s3.js
+++ server/config/s3.js
@@ -4,10 +4,4 @@ var AWS = require('aws-sdk');
 module.exports = function() {
     console.log("AWS configured");
     AWS.config.update({accessKeyId: config.s3.accessKeyId, secretAccessKey: config.s3.secretAccessKey});
-    var s3bucket = new AWS.S3({params: {Bucket: 'asknaturemedia'}});
-    var params = {Key: 'TestYe', Expires: 60, ContentType: 'image/png', ACL: 'private'};
-    s3bucket.getSignedUrl('putObject', params, function(err, data) {
-	console.log(err);
-	console.log(data);
-    });
 }
