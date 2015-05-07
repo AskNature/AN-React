1701d4e0876377a71d18f0db79280b9132c614f0
Ethan Smith <ethan.smith@biomimicry.net>
2015-05-06 18:02:19 -0700

    user detail updates. assorted detail updates

--- client/scripts/components/detail/detail-1user.jsx
+++ client/scripts/components/detail/detail-1user.jsx
@@ -8,6 +8,7 @@ DefaultLayout = require('../layouts/default.jsx'),
 TopSection = require('./common/topsection.jsx'),
 
 RelationshipList = require('./common/relationshiplist.jsx'),
+Gallery = require('./common/gallery.jsx'),
 
 Col = require('react-bootstrap/Col'),
 Row = require('react-bootstrap/Row'),
@@ -25,7 +26,7 @@ var Template = React.createClass({
     }
     var secondaryKey = 'name';
     var descriptionKey = 'special_text';
-    var avatar = data.custom_avatar && data.custom_avatar !== 'null' ? data.custom_avatar : 'http://www.asknature.org/images/uploads/user/'+data.masterid+'/avatar/lg_avatar.jpg';
+    var avatar = data.custom_avatar_url && data.custom_avatar_url !== 'null' ? data.custom_avatar_url : data.masterid ?  'http://www.asknature.org/images/uploads/user/'+data.masterid+'/avatar/lg_avatar.jpg' : '';
     return (
       /* jshint ignore:start */
       <div>
@@ -41,7 +42,78 @@ var Template = React.createClass({
           innerimage={avatar}
           userdetail={true}
           />
+          <Grid>
+            <Row>
 
+              <Col xs={12}>
+
+                  <Gallery items={data} title={data.name} windowHeight={this.props.windowHeight}/>
+                    {this.props.editable ? (
+                      <RelationshipList
+                        items={data.media}
+                        editable={this.props.editable}
+                        titleField='name'
+                        onAdd={this.props.onRelationshipAdd.bind(null, 'media')}
+                        onRemove={this.props.onRelationshipRemove.bind(null, 'media')}
+                        field={'media'}
+                        routeName={'media'}
+                        title={'Personal Gallery'}
+                        fieldName={'Media'}
+                         />
+                    ) : '' }
+
+              </Col>
+              <Col xs={12} sm={6}>
+                <RelationshipList
+                  items={data.bookmarked}
+                  editable={false}
+                  field='content'
+                  title='Bookmarked'
+                  fieldName='bookmarked'
+                  titleField='name' />
+              </Col>
+              <Col xs={12} sm={6}>
+                <RelationshipList
+                  items={data.friends}
+                  editable={false}
+                  titleField='name'
+                  onAdd={this.props.onRelationshipAdd.bind(null, 'friends')}
+                  onRemove={this.props.onRelationshipRemove.bind(null, 'friends')}
+                  field={'Users'}
+                  routeName={'1user'}
+                  title={'Friends'}
+                  fieldName={'Friends'}
+                   />
+              </Col>
+              <Col xs={12}>
+                <RelationshipList
+                  items={data.added_content}
+                  editable={false}
+                  titleField='name'
+                  onAdd={this.props.onRelationshipAdd.bind(null, 'added_content')}
+                  onRemove={this.props.onRelationshipRemove.bind(null, 'added_content')}
+                  field={'media'}
+                  routeName={'media'}
+                  title={'Contributed Content'}
+                  fieldName={'Content'}
+                   />
+              </Col>
+              <Col xs={12}>
+                <RelationshipList
+                  items={data.added_media}
+                  editable={false}
+                  titleField='name'
+                  onAdd={this.props.onRelationshipAdd.bind(null, 'added_media')}
+                  onRemove={this.props.onRelationshipRemove.bind(null, 'added_media')}
+                  field={'media'}
+                  routeName={'media'}
+                  title={'Contributed Media'}
+                  fieldName={'Media'}
+                   />
+              </Col>
+
+            </Row>
+          </Grid>
     </div>
     /* jshint ignore:end */
     );
