8767eba00645cf9c784d3ffebfb3e95a06653008
Ethan Smith <ethan.smith@biomimicry.net>
2015-05-08 16:43:40 -0700

    fix temp idea&insight and description fields in source detail

--- client/scripts/components/detail/detail-source.jsx
+++ client/scripts/components/detail/detail-source.jsx
@@ -37,7 +37,7 @@ var Template = React.createClass({
     var primaryKey = 'name';
     var secondaryKey = 'secondary_title';
     var primaryLinkKey = 'url';
-    var descriptionKey = 'abstract_excerpt';
+    var descriptionKey = 'description';
     var addedby = data.added_by;
     var default_avatar = 'https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xaf1/v/t1.0-9/10383663_869350803096314_2369845013213041061_n.png?oh=2c010ce055331caa73a9506795239fd1&oe=55BDD82A&__gda__=1433772443_f5c43498047b8193dccc0a5554ba6ed1';
     return (
